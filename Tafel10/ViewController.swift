a3f05a16d95090c2a4423fdbcd055cdddd082e01
Andre <andre@c-sync.nl>
2020-12-29 14:24:32 +0100

    Initial Commit

---
+++ Tafel10/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tafel10
+//
+//  Created by Andre Seesink on 29/12/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
