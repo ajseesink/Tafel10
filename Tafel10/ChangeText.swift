6c4f2d24c5a3837831c33f11948a1cae2d984aea
Andre <andre@c-sync.nl>
2021-01-02 02:01:15 +0100

    test version

---
+++ Tafel10/ChangeText.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeText.swift
+//  Tafel10
+//
+//  Created by Andre Seesink on 30/12/2020.
+//
+
+import Foundation
+
+func mySetLabel(tekst: String) -> String {
+    return "Ingekomen \(tekst)"
+}
+
+
--- Tafel10/ViewController.swift
+++ Tafel10/ViewController.swift
@@ -8,19 +8,47 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+   
+    
+    @IBOutlet weak var resetButton: NSButton!
+    @IBOutlet weak var textLabel: NSTextField!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+            
         }
     }
 
-
+   // Reset button is pushed here.
+    @IBAction func pushedResetButton(_ sender: Any) {
+        // var name = textLabel.stringValue
+        // name = "Button Reset is pushed"
+        // textLabel.stringValue = name
+        // textLabel.stringValue = "Button Reset is pushed"
+        setLabel(inputText: "The reset Button is pushed")
+    }
+    
+    // Call this function to change text in label.
+    func setLabel(inputText: String){
+       var name = textLabel.stringValue
+       name = inputText
+       textLabel.stringValue = name
+         }
+    
+    @IBAction func pushedButton2(_ sender: Any) {
+       // setLabel(inputText: "The Button is pushed")
+        let textname = mySetLabel(tekst: "Hello World")
+        setLabel(inputText: textname)
+    }
+    
 }
+
 
